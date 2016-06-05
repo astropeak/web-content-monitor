4d91c735b183fe61e798f15efd82bd3d7b1f39ce
Astropeak <astropeak@163.com>
2016-06-05 14:22:06 +0800

    add parameter

--- web-scraping.js
+++ web-scraping.js
@@ -1,32 +1,38 @@
 var driver = require('node-phantom-simple');
 
-driver.create({ path: require('phantomjs').path }, function (err, browser) {
-    return browser.createPage(function (err, page) {
-        // return page.open("http://tilomitra.com/repository/screenscrape/ajax.html", function (err,status) {
-        return page.open("http://phantomjs.org/examples", function (err,status) {
-            console.log("opened site? ", status);
-            page.injectJs('./js/jquery.min.js', function (err) {
-                // jQuery Loaded.
-                // Wait for a bit for AJAX content to load on the page. Here, we are waiting 5 seconds.
-                setTimeout(function () {
-                    return page.evaluate(function () {
-                        //Get what you want from the page using jQuery. A good way is to populate an object with all the jQuery commands that you need and then return the object.
-                        var h2Arr = [],
-                            pArr = [];
 
-                        $('h2').each(function () { h2Arr.push($(this).html()); });
-                        $('p').each(function () { pArr.push($(this).html()); });
-
-                        return {
-                            h2: h2Arr,
-                            p: pArr
-                        };
-                    }, function (err,result) {
-                        console.log(result);
-                        browser.exit();
-                    });
-                }, 2000);
+function get(url, selector, cb) {
+    // url: "http://phantomjs.org/examples"
+    driver.create({ path: require('phantomjs').path }, function (err, browser) {
+        return browser.createPage(function (err, page) {
+            return page.open(url, function (err,status) {
+                console.log("opened site? ", status);
+                page.injectJs('./js/jquery.min.js', function (err) {
+                    console.log("err: ", err);
+                    console.log("1.selector: ", selector);
+                    // jQuery Loaded.
+                    // Wait for a bit for AJAX content to load on the page. Here, we are waiting 5 seconds.
+                    setTimeout(function () {
+                        return page.evaluate(function (selector1) {
+                            //Get what you want from the page using jQuery. A good way is to populate an object with all the jQuery commands that you need and then return the object.
+                            var eee = [];
+                            $(selector1).each(function () { eee.push($(this).html()); });
+                            return eee;
+                        }, selector, function (err,result) {
+                            cb(err, result);
+                            browser.exit();
+                        });
+                    }, 1000);
+                });
             });
         });
     });
+}
+
+// get("http://phantomjs.org/examples", "h2");
+get("http://www.baidu.com", "p", function (err, result) {
+    console.log("err: ", err);
+    console.log("result: ", result);
 });
+
+module.exports = {'get':get};
